1f36ef5f1f3f89754e0af37b967311a26c47983c
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-06-17 21:04:23 +0900

    手直し

--- main/__init__.py
+++ main/__init__.py
@@ -4,5 +4,3 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('main.config')  # config.pyを読み込んで設定
 db = SQLAlchemy(app)    # dbをオブジェクトにする
-
-import main.views
--- manage.py
+++ manage.py
@@ -1,2 +1,4 @@
 from main import app
+import main.views
+
 app.run(debug=True)
