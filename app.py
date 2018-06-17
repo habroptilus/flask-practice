624369d9b7048b3f3d733902dcdf1c9e17cddbaf
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2018-06-17 17:54:00 +0900

    db連携した

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+
+    def __init__(self, username):
+        self.username = username
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', message="こんにちは")
+    user_list = User.query.all()
+    return render_template('index.html', message="こんにちは", user_list=user_list)
 
 
 @app.route('/test', methods=['GET', 'POST'])
@@ -23,6 +36,16 @@ def test():
 @app.route('/hoge/<postid>')
 def hoge(postid):
     return render_template('hoge.html', result='id = {}'.format(postid))
+
+
+@app.route("/add_user", methods=['POST'])
+def add_user():
+    username = request.form.get('username')
+    if username:
+        user = User(username)
+        db.session.add(user)
+        db.session.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
